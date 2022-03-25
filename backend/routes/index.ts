650af6c3dca134042e572d58e105367abe80123e
dmytro.shklianka <dmytro.shklianka.work@gmail.com>
2022-03-25 16:38:23 +0300

    small change

--- backend/routes/index.ts
+++ backend/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import ProductRouter from "./product.routes";
 import UserRouter from "./user.routes";
+import AdminRouter from "./admin.routes";
 
 const router = Router();
 
 router.use("/product", ProductRouter);
 router.use("/user", UserRouter);
+router.use("/admin", AdminRouter);
 
 export default router;
